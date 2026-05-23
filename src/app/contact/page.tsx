2fa2ccfdf614c1af153b056b85f7817dcd181b39
Z User <z@container>
2026-05-23 03:46:40 +0000

    fix: add pt-12 padding to contact info section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -64,7 +64,7 @@ export default function ContactPage() {
         />
 
         {/* Contact Info Cards */}
-        <section className="pb-12">
+        <section className="pt-12 pb-12">
           <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
               {contactInfo.map((info, i) => (
