fc4097df1f756c3c86def2274324eeba52cac1c7
Z User <z@container>
2026-07-29 07:35:59 +0000

    style: set hero image opacity to 10%

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
         fill
         priority
         sizes="100vw"
-        className="object-cover object-center"
+        className="object-cover object-center opacity-10"
       />
 
       {/* Bottom fade into next section */}
